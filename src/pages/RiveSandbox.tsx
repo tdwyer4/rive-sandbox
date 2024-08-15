8328ba2bc120dbfb0167fa2ff0e7a9eeb211fd48
Tim Dwyer <timdwyer@Tims-MBP.localdomain>
2024-08-15 16:36:08 -0500

    style revisions

--- src/pages/RiveSandbox.tsx
+++ src/pages/RiveSandbox.tsx
@@ -3,15 +3,17 @@ import styles from "./RiveSandbox.module.scss";
 
 export function RiveSandbox() {
   return (
-    <div className={styles.RiveSandbox}>
-      <div className="title-container">
+    <>
+      <div className={styles.riveSandbox}>
         <h1>Early Bird Pricing Interaction</h1>
       </div>
-      <EHRive
-        // filename={"assets/rive/myrivefile.rive"}
-        filename={"assets/rive/ebi-test.riv"}
-        stateMachineName="State Machine 1"
-      />
-    </div>
+      <div>
+        <EHRive
+          // filename={"assets/rive/myrivefile.riv"}
+          filename={"assets/rive/ebi-test.riv"}
+          stateMachineName="State Machine 1"
+        />
+      </div>
+    </>
   );
 }

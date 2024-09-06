7f811254f77abfad93e60bb48b3ec31e77f83228
Tim Dwyer <timdwyer@Tims-MBP.localdomain>
2024-09-06 14:45:21 -0500

    Rive updates

--- src/pages/RiveFile2.tsx
+++ src/pages/RiveFile2.tsx
@@ -3,7 +3,7 @@ import styles from "./RiveSandbox.module.scss";
 
 export function RiveFileTwo() {
   return (
-    <>
+    <div className={styles.riveContainer}>
       <div className={styles.riveSandbox}>
         <h1>Brand New Designs</h1>
       </div>
@@ -14,6 +14,6 @@ export function RiveFileTwo() {
           stateMachineName="State Machine 1"
         />
       </div>
-    </>
+    </div>
   );
 }
--- src/pages/RiveSandbox.tsx
+++ src/pages/RiveSandbox.tsx
@@ -3,7 +3,7 @@ import styles from "./RiveSandbox.module.scss";
 
 export function RiveSandbox() {
   return (
-    <>
+    <div className={styles.riveContainer}>
       <div className={styles.riveSandbox}>
         <h1>Early Bird Pricing</h1>
       </div>
@@ -14,6 +14,6 @@ export function RiveSandbox() {
           stateMachineName="State Machine 1"
         />
       </div>
-    </>
+    </div>
   );
 }
